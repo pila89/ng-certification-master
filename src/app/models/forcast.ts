9c1d0077b52e4b1e9d74d4d07c5f039c8a96fba3
pila89 <salim.amara@outlook.com>
2023-01-31 00:02:11 +0100

    fianl

--- src/app/models/forcast.ts
+++ src/app/models/forcast.ts
@@ -1,8 +1,8 @@
 export interface Forcast {
-  essage?: number;
+  cod?: string;
+  message?: number;
   cnt?: number;
-  liscod?: string;
-  mt?: [
+  list?: [
     {
       dt?: number;
       main?: {
@@ -30,7 +30,7 @@ export interface Forcast {
   city?: {
     id?: number;
     name?: string;
-    coord?: { lat?: number; lon?: number };
+    coord?: { lat: number; lon: number };
     country?: string;
     population?: number;
     timezone?: number;
@@ -38,46 +38,3 @@ export interface Forcast {
     sunset?: number;
   };
 }
-
-
-
-// export interface Forcast {
-//     city: {
-//       id?: number;
-//       name?: string;
-//       coord?: { lon: number; lat: number };
-//       country?: string;
-//       population?: number;
-//       timezone?: number;
-//     };
-//     cod?: string;
-//     message?: number;
-//     cnt?: number;
-//     list?: [
-//       {
-//         dt?: number;
-//         sunrise?: number;
-//         sunset?: number;
-//         temp?: {
-//           day?: number;
-//           min?: number;
-//           max?: number;
-//           night?: number;
-//           eve?: number;
-//           morn?: number;
-//         };
-//         feels_like?: { day: number; night: number; eve: number; morn: number };
-//         pressure?: number;
-//         humidity?: number;
-//         weather?: [
-//           { id?: number; main?: string; description?: string; icon?: string }
-//         ];
-//         speed?: number;
-//         deg?: number;
-//         gust?: number;
-//         clouds?: number;
-//         pop?: number;
-//       }
-//     ];
-//   }
-  
